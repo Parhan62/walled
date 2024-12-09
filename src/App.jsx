d6b5c77a8be5c6c63cf6b09e63b8199edfbc6c7c
parhan <parhansuryana1@gmail.com>
2024-12-09 15:01:33 +0700

    menyalin kode damar

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,13 @@
-import NavItems from './components/NavItems'
-import logo from './assets/Vector.png'
-import { useState } from 'react'
-// import './App.css'
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
 
 function App() {
-  const [activeTab, setActiveTab] = useState("Dashboard")
-  const menu = [
-    {title: "Dashboard",
-    Link: "#"
-    },
-    {title: "Transfer",
-      Link: "#"
-    },
-    {title: "Topup",
-      Link: "#"
-    },
-    {title: "Sign Out",
-      Link: "#"
-    }
-  ]
-
   return (
-    <>
-      <nav className='flex items-center w-screen justify-between bg-white py-4 px-8 font bold'>
-        <img src = {logo}/>
-        <div className='flex gap-x-8'>
-          <NavItems
-            menu={menu}
-            activeTab={activeTab}
-            handleClick={setActiveTab} />
-        </div>
-      </nav>
-    </>
-  )
+    <main>
+      <Navbar />
+      <Hero />
+    </main>
+  );
 }
 
-export default App
+export default App;
